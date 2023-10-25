37ba1d7cb9f8c478506c3488af1ee940eb894304
Aaron Edaku <eaaron25@gmail.com>
2023-10-25 16:12:23 +0000

    added livewire comp

---
+++ app/Livewire/GameCode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class GameCode extends Component
+{
+    public function render()
+    {
+        return view('livewire.game-code');
+    }
+}
