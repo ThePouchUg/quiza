7f62f752d9e74fadfba3891918a2c8fb0b647766
Aaron Edaku <eaaron25@gmail.com>
2023-10-26 13:32:07 +0000

    added tailwind css script

--- resources/views/livewire/game-code.blade.php
+++ resources/views/livewire/game-code.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="flex flex-col h-screen items-center justify-center border-4 ">
+    <h1 class="text-3xl font-bold">The Pub Quiz</h1>
+    <h2 class="text-2xl">Enter quiz code</h2>
+    <form action="">
+        <div class="">
+            <div>
+                <label for="quiz_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quiz
+                    Code</label>
+                <input type="text" id="first_name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="EF002255" required>
+            </div>
+            <input type="button" value="Enter"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        </div>
+    </form>
 </div>
