b417efd8ac588865c59a95580cb9603e480b3c8b
Aaron Edaku <eaaron25@gmail.com>
2023-10-26 14:49:36 +0000

    hide label

--- resources/views/livewire/game-code.blade.php
+++ resources/views/livewire/game-code.blade.php
@@ -3,10 +3,10 @@
     <h2 class="text-2xl">Enter quiz code</h2>
     <form action="">
         <div class="">
-            <div>
-                <label for="quiz_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quiz
+            <div class="flex flex-col justify-center items-center">
+                <label for="quiz-code" class="block mb-2 text-sm font-medium text-gray-900 sr-only">Quiz
                     Code</label>
-                <input type="text" id="first_name"
+                <input type="text" id="quiz-code"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     placeholder="EF002255" required>
             </div>
