d3c24a3c173d514b853b9bb3ddf623bac2700d80
Aaron Edaku <eaaron25@gmail.com>
2023-11-08 09:52:59 +0000

    added navbar

--- resources/views/livewire/create-quiz.blade.php
+++ resources/views/livewire/create-quiz.blade.php
@@ -1,3 +1,15 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
+
+    <form action="" method="post">
+        <input type="button" value="create">
+    </form>
+    {{-- The Master doesn't talk, he acts. --}}
+
+    {{-- The Master doesn't talk, he acts. --}}
+
+    {{-- The Master doesn't talk, he acts. --}}
+
+    {{-- The Master doesn't talk, he acts. --}}
+
 </div>
